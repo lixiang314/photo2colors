c17f184a49a34d047fc7fd8904169a6452cc3fb5
条件状语从句 <lixzap@gmail.com>
2017-07-28 05:23:22 +0800

    dev

--- public/js/index.js
+++ public/js/index.js
@@ -1,31 +1,41 @@
-
 $("#input-img").uploadPreview({
-    Img: "img-show",
-    ImgType: ["png", "jpg", "bmp"],
-    Callback: function() {
-      showColor();
-    }
-  });
+  Img: "img-show",
+  ImgType: ["png", "jpg", "bmp"],
+  Callback: function() {
+    $('.cover').show();
+    var _src = $('#img-show').attr('src');
+    console.log(_src);
+    $('.img-main').css('background','url("'+_src+'") no-repeat center').css('backgroundSize','cover');
+    showColor();
+  }
+});
 
+function loadingOk() {
+  $('.cover').hide();
+  $('.sec-1').hide();
+  $('.sec-2').show();
 
+}
 function showColor() {
   var img = document.getElementById('img-show');
   var colors = RGBaster.colors(img, {
-    paletteSize: 100,
-    success: function(colors){
+    paletteSize: 500,
+    success: function(colors) {
+      loadingOk();
+
       img.style.borderColor = colors.palette[0];
 
-      var makeDivWithClassAndBGColor = function(klass, color){
+      var makeDivWithClassAndBGColor = function(klass, color) {
         var div = document.createElement("div");
         div.className = klass;
         div.style.backgroundColor = color;
         return div;
       };
-      var colorList =[];
+      var colorList = [];
       for (i in colors.palette) {
-
-          colorList.push('<div class="div-color" style="background-color:'+colors.palette[i]+';"></div>');
-
+        if ((Number(i) + 1) % 10 == 0) {
+          colorList.push('<div class="div-color" style="background-color:' + colors.palette[i] + ';" value="' + colors.palette[i] + '"></div>');
+        }
 
       }
       $('#color-list').html(colorList.join('\n'));
@@ -42,3 +52,34 @@ function showColor() {
   });
 
 }
+
+
+$('#pre-step').on('click',function(){
+  $('.sec-3').hide();
+  $('.sec-2').show();
+})
+
+$('#color-list').on('click','.div-color',function(){
+  if ($(this).hasClass('selected')) {
+    return;
+  }
+  var _newBox = '<div class="selected-box" style="background-color:' + $(this).attr("value") + ';"></div>';
+  $('.preview').append(_newBox);
+    $(this).addClass('selected').html('<img src="img/icon-ok.png" alt="" class="title">');
+});
+
+// 重选
+$('.btn-reset').on('click',function() {
+  $('.preview').html('');
+  $('.div-color').html('').removeClass('selected');
+})
+
+$('.btn-next').on('click',function() {
+  $('.sec-2').hide();
+  $('.sec-3').show();
+})
+$('#btn-again').on('click',function() {
+  $('.sec-3').hide();
+  $('.sec-2').hide();
+  $('.sec-1').show();
+})
